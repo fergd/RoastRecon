ccf447bc41dc922da6a22de2771fb74b3a691707
Christan Fergus <fergusdesignllc@gmail.com>
2017-09-17 20:35:25 -0600

    added some jquery to detect devices for serving the right download link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        }
+    };
 
+    if (isMobile.Android()) {
+        $(".cta-container").append("<button class=\"cta-button\">Download for Android</button>");
+    } else if (isMobile.iOS()) {
+        $(".cta-container").append("<button class=\"cta-button\">Download for iOS</button>");
+    } else {
+        $(".cta-container").append("<button class=\"cta-button\">Download for Gen</button>");
+    }
+});
