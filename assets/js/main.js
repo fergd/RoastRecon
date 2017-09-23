1cb356286e8b6db8e1a9db35930ed5796d5abdc6
Christan Fergus <fergusdesignllc@gmail.com>
2017-09-22 22:30:27 -0600

    major layout work, typography, and general progression

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
             return navigator.userAgent.match(/iPhone|iPad|iPod/i);
         }
     };
-
+    
     if (isMobile.Android()) {
-        $(".cta-container").append("<button class=\"cta-button\">Download for Android</button>");
+        $(".cta-container").append("<button class=\"cta-button\">Download for Android</button><br><a href=\"\">Get the iOS version</a>");
     } else if (isMobile.iOS()) {
-        $(".cta-container").append("<button class=\"cta-button\">Download for iOS</button>");
+        $(".cta-container").append("<button class=\"cta-button\">Download for iOS</button><br><a href=\"\">Get the Android version</a>");
     } else {
         $(".cta-container").append("<button class=\"cta-button\">Download for Gen</button>");
     }
