4c72e2a6bc7a6da08fdaa65e2892e9741c308fbc
Christan Fergus <fergusdesignllc@gmail.com>
2017-09-23 20:52:57 -0600

    added some test images for design, modded fonts, layout is honing in

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
     };
     
     if (isMobile.Android()) {
-        $(".cta-container").append("<button class=\"cta-button\">Download for Android</button><br><a href=\"\">Get the iOS version</a>");
+        $(".cta-container").append("<img src=\"../assets/images/google.png\"><br><a href=\"\">Get the iOS version</a>");
     } else if (isMobile.iOS()) {
-        $(".cta-container").append("<button class=\"cta-button\">Download for iOS</button><br><a href=\"\">Get the Android version</a>");
+        $(".cta-container").append("<img src=\"../assets/images/apple.png\"><br><a href=\"\">Get the Android version</a>");
     } else {
-        $(".cta-container").append("<button class=\"cta-button\">Download for Gen</button>");
+        $(".cta-container").append("<img src=\"../assets/images/apple.png\">&nbsp;<img src=\"../assets/images/google.png\">");
     }
 });
